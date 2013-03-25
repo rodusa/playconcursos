fe2990b827df5e123580eb7374e42275ca9b5e5f
Rodrigo Lueneberg <rlueneberg@gmail.com>
2013-03-25 16:33:22 -0300

    Initial Commit

---
+++ playconcursos/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playconcursos::Application.config.secret_token = '2521ec529f616b987b50846c6c0160fd68be7fd3fd4b6ce50fed4faf44df4d5906233144be8f0c602608fc0f16906f92b8448ce57b2681ad087f9f3c98de9468'
